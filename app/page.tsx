fde1c6c00a9a54dcec934133cd50d51f66eac490
Mayur7862 <83858995+Mayur7862@users.noreply.github.com>
2025-03-21 00:14:44 +0530

    Delete some

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Welcome to My Next.js App!</h1>
-    </main>
-  );
-}
